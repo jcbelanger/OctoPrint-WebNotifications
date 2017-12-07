8e45bec95429bdfe5ce98359d2affcacff080c43
Joshua Belanger <jcbelanger@users.noreply.github.com>
2017-12-07 00:24:45 -0500

    lets encrypt registration

--- le.sh
+++ le.sh
@@ -2,35 +2,69 @@
 
 set -e
 
+
 if [ $# -ne 1 ]; then
-	echo $0: usage: le domain
+	echo $0: "usage: le domain"
 	exit 1
 fi
-
 DOMAIN=$1
 
-echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list.d/raspi.list
-apt-get update
-apt-get install \
-	--no-install-recommends \
-	--assume-yes \
-	--force-yes \
-	--target-release jessie-backports \
-	certbot
 
-service haproxy stop
+if ! [ -x "$(command -v certbot)" ]; then
+	echo "Installing certbot from jessie-backports..."
+	
+	BACK_PORTS="deb http://ftp.debian.org/debian jessie-backports main"
+	SOURCES="/etc/apt/sources.list.d/raspi.list"
+	if ! grep --quiet "^$BACK_PORTS" $SOURCES /etc/apt/sources.list.d/*; then
+		echo "Adding jessie-backports"
+		echo "$BACK_PORTS" >> $SOURCES
+	fi
 
-certbot certonly \
-	--standalone \
-	--agree-tos \
-	--non-interactive \
-	--register-unsafely-without-email \
-	--domain $DOMAIN \
-	--domain www.$DOMAIN \
-	--preferred-challenges http \
-	--http-01-port 80
+	apt-get update
+	apt-get install \
+		--no-install-recommends \
+		--assume-yes \
+		--force-yes \
+		--target-release jessie-backports \
+		certbot
+	
+	echo "Installed certbot from jessie-backports!"
+fi
 
-cd /etc/letsencrypt/live/$DOMIN
-cat fullchain.pem privkey.pem > /etc/ssl/snakeoil.pem
 
-service haproxy start
+if false; then
+	echo "Registering $DOMAIN with letsencrypt..."
+	
+	#We stop haproxy to run the certbot as a acme server on port 80
+	#TODO update haproxy config to forward acme requests to certbot
+	service haproxy stop
+	
+	certbot certonly \
+		--standalone \
+		--agree-tos \
+		--non-interactive \
+		--register-unsafely-without-email \
+		--domain $DOMAIN \
+		--domain www.$DOMAIN \
+		--preferred-challenges http \
+		--http-01-port 80
+
+	service haproxy start
+	
+	echo "Registered $DOMAIN with letsencrypt!"
+fi
+
+
+#TODO modify haproxy config instead of overwriting the included octopi self signed cert
+HAPROXY_CERT="/etc/ssl/snakeoil.pem"
+LETS_ENCRYPT_CERT="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
+LETS_ENCRYPT_PRIV_KEY="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
+if [ -f $LETS_ENCRYPT_CERT] && [ -f $LETS_ENCRYPT_CERT]; then
+	echo "Applying letsencrypt cert to haproxy..."
+	rm --force $HAPROXY_CERT
+	cat $LETS_ENCRYPT_CERT  $LETS_ENCRYPT_PRIV_KEY > $HAPROXY_CERT
+	service haproxy reload
+fi
+
+
+#TODO create cronjob to autorenew cert
